db216f64ec8b4efada81eff3947a6dec82236ca7
josip-pavlovic-dev <jolepavlovic@outlook.com>
2026-09-13 11:26:51 +0200

    Add endpoints to filter books by category and author with query parameters

--- fast-api-course-my-work/Project_1/books.py
+++ fast-api-course-my-work/Project_1/books.py
@@ -28,3 +28,27 @@ async def read_book(book_title: str):
     for book in BOOKS:
         if book.get("title").casefold() == book_title.casefold():  # type: ignore
             return book
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = list()
+    for book in BOOKS:
+        if book["category"].casefold() == category.casefold():  # type: ignore
+            books_to_return.append(book)
+    return books_to_return
+
+
+# NAPOMENA: FastAPI automatski parsira query parametre iz URL-a. Na primer, /books/?category=science će pozvati read_category_by_query sa category="science".
+
+
+@app.get("/books/{book_author}/")
+async def read_author_category_by_query(book_author: str, category: str):
+    books_to_return: list[str] = list()
+    for book in BOOKS:
+        if (
+            book.get("author").casefold() == book_author.casefold()  # type: ignore
+            and book.get("category").casefold() == category.casefold()  # type: ignore
+        ):
+            books_to_return.append(book)  # type: ignore
+    return books_to_return
