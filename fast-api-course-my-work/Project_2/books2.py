b5de4ea33d2e7740d819d0f5b6c15e6638b3b226
josip-pavlovic-dev <jolepavlovic@outlook.com>
2026-09-15 03:25:55 +0200

    Add BookRequest model for creating books with validation and update create_book endpoint

--- fast-api-course-my-work/Project_2/books2.py
+++ fast-api-course-my-work/Project_2/books2.py
@@ -1,4 +1,5 @@
-from fastapi import Body, FastAPI
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
@@ -17,6 +18,33 @@ class Book:
         self.description = description
         self.rating = rating
 
+
+class BookRequest(BaseModel):
+    id: int | None = Field(
+        description="The ID of the book is not needed on creation", default=None
+    )
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "Nova Knjiga",
+                "author": "Marko Vidojkovic",
+                "description": "Opis knjige",
+                "rating": 5,
+            }
+        }
+    }
+
+
+# Poredjenja brojeva:
+# lt - less than (manje od)
+# gt - greater than (veće od)
+# le - less than or equal to (manje ili jednako)
+# ge - greater than or equal to (veće ili jednako)
 
 BOOKS: list[Book] = [
     Book(
@@ -64,11 +92,49 @@ BOOKS: list[Book] = [
 ]
 
 
+# Čitanje svih knjiga:
 @app.get("/books")
 async def read_all_books():
     return BOOKS
 
 
-@app.post("/books/create-book")
-async def create_book(book_request=Body(...)):
-    BOOKS.append(book_request)
+# Čitanje pojedinačne knjige po ID-u:
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    return {"error": "Book not found"}
+
+
+# Kreiranje nove knjige:
+@app.post("/create-book", status_code=201)
+async def create_book(book_request: BookRequest):
+    new_book = Book(
+        **book_request.model_dump()
+    )  # konvertuje zahtev u objekat klase Book
+    BOOKS.append(find_book_id(new_book))  # type: ignore
+
+    return new_book
+
+
+# Funkcija koja omogućava da id knjige ide redom kako ih registrujemo u listi BOOKS
+def find_book_id(book: Book):
+
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    # if len(BOOKS) > 0:
+    #     book.id = BOOKS[-1].id + 1
+    # else:
+    #     book.id = 1
+
+    return book
+
+
+# Deo po deo `new_book= Book(**book_request.model_dump())`:
+# 1. `book_request.model_dump()` konvertuje Pydantic model u rečnik.
+# 2. `**book_request.model_dump()` raspakuje rečnik u ključne (keyword arguments naprimer `id=1, title="FastAPI for Beginners", ...`) argumente za konstruktor klase Book.
+# 3. `Book(**book_request.model_dump())` kreira novi objekat klase Book sa podacima iz zahteva.
+
+# Šta tačno radi funkcija ‚model_dump()‘:
+# `model_dump()` je metoda Pydantic modela koja vraća podatke modela kao rečnik.
+# Ovo omogućava jednostavno kreiranje novih objekata koristeći te podatke.
